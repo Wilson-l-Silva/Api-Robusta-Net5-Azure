4802997bd92ca5d33a91c00e33685336646be32d
wlima <wlima@osas.com.br>
2024-11-02 10:55:43 -0300

    Adicionado classe de contexto para acessar o entity framework core. Instalado pacote EntityFrameworkCore 5.0.1

---
+++ Manager.Infra/Context/ManagerContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Manager.Infra.Context
+{
+    public class ManagerContext : DbContext
+    {
+        public ManagerContext()
+        {
+            
+        }
+
+    }
+}
