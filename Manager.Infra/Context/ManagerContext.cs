4c778f138bd7590e7084b0f2f7908bf9ed18b6ba
wlima <wlima@osas.com.br>
2024-11-04 07:47:27 -0300

    Adicionado classe de ContextoManager para conectar no banco, Adicionado classe de UserMap para de para da classe do usuário e o banco, Adicionado classe UserReposity. Feito as primeiras migrations para criar e alterar a tabela no banco.

--- Manager.Infra/Context/ManagerContext.cs
+++ Manager.Infra/Context/ManagerContext.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Manager.Domain.Entities;
+using Manager.Infra.Mappings;
 using Microsoft.EntityFrameworkCore;
 
 namespace Manager.Infra.Context
@@ -11,8 +13,26 @@ namespace Manager.Infra.Context
     {
         public ManagerContext()
         {
-
+           
             
         }
+
+        public ManagerContext(DbContextOptions<ManagerContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=OSS-BH-NBT435;Initial Catalog=USERMANAGERAPI;User Id=sa;Password=x; Integrated Security=true;");
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new UserMap());
+        }
+
     }
 }
